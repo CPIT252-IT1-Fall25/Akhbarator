acf55b4cb38238f971cf113860afc66b6b8168db
Hamad Alharbi <Finalonhamad@gmail.com>
2025-11-09 13:51:31 +0300

    add classes (Article) and (ArticleHandler)

---
+++ src/main/java/sa/edu/kau/fcit/cpit252/project/Article.java
@@ -0,0 +1,10 @@
+package sa.edu.kau.fcit.cpit252.project;
+
+import java.util.Date;
+
+public class Article {
+    public String author;
+    public Date date;
+    public String content;
+    public int priority;
+}

f9fe7b09b0a96a09d12cb939250f1f8f37b687d1
AbSaq <abdulrahman.salem.akeel@gmail.com>
2025-11-12 23:37:15 +0300

    TASK: adjusted dependancies and restructured some files.

--- src/main/java/sa/edu/kau/fcit/cpit252/project/news/AuthorsBlackList.java
+++ src/main/java/sa/edu/kau/fcit/cpit252/project/news/AuthorsBlackList.java
@@ -3,7 +3,10 @@ package sa.edu.kau.fcit.cpit252.project.news;
 import sa.edu.kau.fcit.cpit252.project.news.Article;
 import sa.edu.kau.fcit.cpit252.project.news.ArticleHandler;
 
+import java.util.ArrayList;
+
 public class AuthorsBlackList extends ArticleHandler {
+    ArrayList<String> blacklist = new ArrayList<String>();
     @Override
     protected Article processArticle(Article article) {
         return article;
